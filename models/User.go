d1b67b0fbca81992e3afd7b37c7308864be912bf
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-09-16 10:55:34 -0300

    models

--- models/User.go
+++ models/User.go
@@ -1,20 +1,24 @@
+// Package models define all data types of application
 package models
 
 import (
 	"github.com/codeYann/web-chat/utils"
 )
 
+// UserInterface export all functions that User must have
 type UserInterface interface {
 	GetAllUsers() []User
 	GetUserById(ID int) User
 }
 
+// User struct defines
 type User struct {
 	Name     string `json:"Name"`
 	Nickname string `json:"Nickname"`
 	Password string `json:"-"`
 	ID       int    `json:"ID"`
 }
+
 
 func CreateUser(name, nickName, password string) *User {
 	return &User{
