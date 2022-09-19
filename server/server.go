825883e1b06b3a238a76b1462a28057a2bf93c8f
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-09-19 10:44:16 -0300

    It adding net, log, settings, and models package. Running server on localhost:3333

--- server/server.go
+++ server/server.go
@@ -1,8 +1,28 @@
+// Package server implements a http server using the go standard lib.
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-// Run is a entry point to run this application
+	"github.com/codeYann/web-chat/models"
+	"github.com/codeYann/web-chat/settings"
+)
+
+var apiSettings *settings.APIConfig = settings.ExportAPIConfig()
+
+func createHTTPServer() {
+	log.Println(models.GetAllUsers())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s", "this is my first web server "+r.URL.Path)
+	})
+
+	http.ListenAndServe(apiSettings.Port, nil)
+}
+
+// Run function exec the web server.
 func Run() {
-	fmt.Println("Hi there :) 👽👽")
+	log.Printf("Server Running on Localhost%s", apiSettings.Port)
+	createHTTPServer()
 }
