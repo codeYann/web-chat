8db1c2a228653d71db58a5cf29d9848bdeb31981
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-09-20 17:37:07 -0300

    User controller

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,4 +1,21 @@
-// Package controllers must blab la
+// Package controllers must interact with models and export its data in JSON format.
 package controllers
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
 
+	"github.com/codeYann/web-chat/models"
+)
+
+// Users returns all users in JSON format.
+// It uses models.GetAllUsers function to get every possible user row in the database.
+func Users(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	allUsers := models.GetAllUsers()
+	err := json.NewEncoder(w).Encode(allUsers)
+	if err != nil {
+		log.Fatal("It's not possible to send json")
+	}
+}
