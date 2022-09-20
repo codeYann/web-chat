30505004dfa5e27813e60ec8308ebffee405da66
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-09-20 17:36:40 -0300

    Implementing GetAllUsers method

--- models/User.go
+++ models/User.go
@@ -27,36 +27,46 @@ func CreateUsers(id int, name, email, passowrd, nickame string) *Users {
 	}
 }
 
-// GetAllUsers returns a list of all users.
-func GetAllUsers() *Users {
-	var user Users
+// GetAllUsers returns a list of users gets by 'SELECT * FROM users' query.
+func GetAllUsers() []Users {
+	usersList := make([]Users, 0)
 
 	connection, err := database.OpenConnection()
 	if err != nil {
-		log.Fatal("Unable to connect database")
+		log.Fatal("Unable to connect to the database")
 	}
+
+	var (
+		ID       int
+		Name     string
+		Email    string
+		Password string
+		Nickname string
+	)
 
 	defer connection.Close()
 
 	response, err := connection.Query(`SELECT * FROM users`)
 	if err != nil {
-		log.Fatal("It cannot run SELECT * FROM users query")
+		log.Fatal("It isn't possible to run 'SELECT * FROM useres' ")
 	}
 
 	defer response.Close()
 
 	for response.Next() {
 		err := response.Scan(
-			&user.ID,
-			&user.Name,
-			&user.Email,
-			&user.password,
-			&user.Nickname,
+			&ID,
+			&Name,
+			&Email,
+			&Password,
+			&Nickname,
 		)
 		if err != nil {
 			log.Fatal("Error on iterate over rows returned by 'SELECT * FROM users' query.")
 		}
-	}
+		user := CreateUsers(ID, Name, Email, Password, Nickname)
 
-	return &user
+		usersList = append(usersList, *user)
+	}
+	return usersList
 }
