2a8ce0d3055039da87aaa6d7d7d8bacab0b0a7fd
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-09-23 13:39:21 -0300

    Change GetAllUsers and GetUserByID to FindAll and FindOne, Implement Store method

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -5,20 +5,55 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/codeYann/web-chat/models"
+	"github.com/gorilla/mux"
 )
 
 // Users returns all users in JSON format.
-// It uses models.GetAllUsers function to get every possible user row in the database.
+// It uses models.FindAll() function to get every possible user row in the database.
 func Users(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 
-	allUsers := models.GetAllUsers()
+	allUsers := models.FindAll()
 
-	err := json.NewEncoder(w).Encode(allUsers)
-	if err != nil {
+	if err := json.NewEncoder(w).Encode(allUsers); err != nil {
 		log.Fatal("It's not possible to send json")
 	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// UserByID returns a single user in JSON format.
+// It uses models.FindOne() to get a this single user.
+func UserByID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	userID, _ := strconv.Atoi(params["ID"])
+
+	user := models.FindOne(uint64(userID))
+
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		log.Fatal("It's not possible to send json")
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// StoreUser insert a new user to the database.
+// It uses models.Save() to store a new user.
+func StoreUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		log.Fatal("Error on decode body params")
+	}
+
+	models.Save(user)
+
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		log.Fatal("Error on Encode User data")
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
