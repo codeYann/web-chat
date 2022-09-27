ed6dd6a275b56544853d91ca2a3f329246e3c5bf
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-09-27 13:54:37 -0300

    Removing implementation logics and just export a user and their methods

--- models/User.go
+++ models/User.go
@@ -1,13 +1,6 @@
 // Package models define structs to map database tables.
 package models
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/codeYann/web-chat/database"
-)
-
 // User struct defines all information User must have.
 type User struct {
 	ID       int    `json:"id"`
@@ -17,130 +10,14 @@ type User struct {
 	Nickname string `json:"nickname"`
 }
 
-// Users type is a list of user
+// Users type is a list of user.
 type Users []User
 
-// CreateUser return a pointer to a new user
-func CreateUser(id int, name, email, password, nickame string) *User {
-	return &User{
-		ID:       id,
-		Name:     name,
-		Email:    email,
-		Password: password,
-		Nickname: nickame,
-	}
-}
-
-// FindAll returns a list of users gets by 'SELECT * FROM users' query.
-func FindAll() Users {
-	var usersList Users
-
-	connection, err := database.OpenConnection()
-	if err != nil {
-		log.Fatal("Unable to connect to the database")
-	}
-
-	defer connection.Close()
-
-	usersResponseChan := make(chan User)
-
-	go func() {
-		response, err := connection.Query(`SELECT * FROM users`)
-		if err != nil {
-			log.Fatal("It's not possible to run 'SELECT * FROM useres' ")
-		}
-
-		defer response.Close()
-
-		for response.Next() {
-			var user User
-
-			err := response.Scan(
-				&user.ID,
-				&user.Name,
-				&user.Email,
-				&user.Password,
-				&user.Nickname,
-			)
-			if err != nil {
-				log.Fatal("Error on iterate over rows returned by 'SELECT * FROM users' query.")
-			}
-
-			usersResponseChan <- user
-		}
-
-		close(usersResponseChan)
-	}()
-
-	for user := range usersResponseChan {
-		usersList = append(usersList, user)
-	}
-
-	return usersList
-}
-
-// FindOne returns a single user get by "SELECT * FROM users WHERE users.id = id"
-func FindOne(ID uint64) User {
-	var user User
-
-	connection, err := database.OpenConnection()
-	if err != nil {
-		log.Fatal("Unable to connect to the database")
-	}
-
-	defer connection.Close()
-
-	query := fmt.Sprintf(`SELECT * FROM users WHERE users.id = %d`, ID)
-
-	response, err := connection.Query(query)
-	if err != nil {
-		log.Fatal("It's not possible to run 'SELECT * FROM useres WHERE users.id = /users/{id}'")
-	}
-
-	defer response.Close()
-
-	for response.Next() {
-
-		err := response.Scan(
-			&user.ID,
-			&user.Name,
-			&user.Email,
-			&user.Password,
-			&user.Nickname,
-		)
-		if err != nil {
-			log.Fatal("Error on iterate over rows returned by 'SELECT * FROM users' query.")
-		}
-
-	}
-
-	return user
-}
-
-// Save function creates a new user in the database.
-func Save(user User) {
-	connection, err := database.OpenConnection()
-	if err != nil {
-		log.Fatal("Failed on connection to the database")
-	}
-
-	defer connection.Close()
-
-	query := fmt.Sprintf(
-		`INSERT INTO users
-	       (name, email, password, nickname)
-     VALUES
-	       ('%s', '%s', '%x', '%s')`,
-		user.Name,
-		user.Email,
-		user.Password,
-		user.Nickname,
-	)
-
-	response, err := connection.Query(query)
-	if err != nil {
-		log.Fatal(`It cannot run INSERT INTO query`)
-	}
-
-	defer response.Close()
+// UserRepository defines all operations User must have.
+type UserRepository interface {
+	FindAll() (Users, error)
+	FindOne(ID uint64) (User, error)
+	SaveOne(user User) (User, error)
+	UpdateOne(ID uint64, nickname string) (User, error)
+	DeleteOne(ID uint64) (User, error)
 }
